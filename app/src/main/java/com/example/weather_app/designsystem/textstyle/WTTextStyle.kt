5d78ed94852aa231eb1b8eccd29e3a8072ef536b
yasse <yassenyasooo50@gmail.com>
2026-03-04 23:30:09 +0200

    add font styles and add function to choose the font family depend on language

---
+++ app/src/main/java/com/example/weather_app/designsystem/textstyle/WTTextStyle.kt
@@ -0,0 +1,15 @@
+package com.example.weather_app.designsystem.textstyle
+
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.text.TextStyle
+
+data class WTTextStyle(
+    val headline: TextStyle,
+    val title: TextStyle,
+    val bodyLarge: TextStyle,
+    val bodyMedium: TextStyle,
+    val hint: TextStyle
+)
+val LocalWTTypography = staticCompositionLocalOf {
+    getWTTypography(isArabic = false)
+}
