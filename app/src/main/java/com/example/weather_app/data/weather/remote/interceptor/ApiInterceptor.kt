765e786696fc64ed5846987ba53a6092c18899e5
yasse <yassenyasooo50@gmail.com>
2026-03-05 09:58:36 +0200

    add api key interceptor and language interceptor

---
+++ app/src/main/java/com/example/weather_app/data/weather/remote/interceptor/ApiInterceptor.kt
@@ -0,0 +1,17 @@
+package com.example.weather_app.data.weather.remote.interceptor
+
+import com.example.weather_app.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.Response
+
+
+class ApiInterceptor: Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+        val url = originalRequest.url.newBuilder()
+            .addQueryParameter("appid", BuildConfig.WEATHER_API_KEY)
+            .build()
+        val newRequest = originalRequest.newBuilder().url(url).build()
+        return chain.proceed(newRequest)
+    }
+}
