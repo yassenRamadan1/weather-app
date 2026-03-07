20983a905ad9cbc83afcc5e881fc9cb597937bb5
yasse <yassenyasooo50@gmail.com>
2026-03-07 21:21:40 +0200

    add app data base

---
+++ app/src/main/java/com/example/weather_app/data/database/AppDataBae.kt
@@ -0,0 +1,15 @@
+package com.example.weather_app.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.weather_app.data.weather.local.WeatherDao
+import com.example.weather_app.data.weather.local.entity.WeatherEntity
+
+@Database(
+    entities = [WeatherEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun weatherDao(): WeatherDao
+}
