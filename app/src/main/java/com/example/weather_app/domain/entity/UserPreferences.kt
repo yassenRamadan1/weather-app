37e43dc710b841c0c7549623698932edfd902bd4
yasse <yassenyasooo50@gmail.com>
2026-03-10 01:46:51 +0200

    change update locale from context from domain to design system

--- app/src/main/java/com/example/weather_app/domain/entity/UserPreferences.kt
+++ app/src/main/java/com/example/weather_app/domain/entity/UserPreferences.kt
@@ -1,7 +1,5 @@
 package com.example.weather_app.domain.entity
 
-import android.content.Context
-import android.content.res.Configuration
 import java.util.Locale
 
 
@@ -47,17 +45,3 @@ enum class LocationMode {
     GPS, MAP
 }
 
-fun Context.updateLocale(locale: Locale): Context {
-    val config = Configuration(resources.configuration)
-    config.setLocale(locale)
-    config.setLayoutDirection(locale)
-    val localizedContext = createConfigurationContext(config)
-    return object : android.content.ContextWrapper(this) {
-        override fun getResources(): android.content.res.Resources = localizedContext.resources
-        override fun getAssets(): android.content.res.AssetManager = localizedContext.assets
-        override fun getSystemService(name: String): Any? = localizedContext.getSystemService(name)
-        override fun createConfigurationContext(overrideConfiguration: Configuration): Context {
-            return localizedContext.createConfigurationContext(overrideConfiguration)
-        }
-    }
-}
