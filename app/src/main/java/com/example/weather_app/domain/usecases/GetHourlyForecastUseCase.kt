702763bb5033a7aa8ee0f27b17c872e4077c9ed5
yasse <yassenyasooo50@gmail.com>
2026-03-09 23:39:27 +0200

    implement get hourly and daily forecast use cases

---
+++ app/src/main/java/com/example/weather_app/domain/usecases/GetHourlyForecastUseCase.kt
@@ -0,0 +1,12 @@
+package com.example.weather_app.domain.usecases
+
+import com.example.weather_app.domain.entity.HourlyWeather
+import com.example.weather_app.domain.repository.WeatherRepository
+import kotlinx.coroutines.flow.Flow
+
+class GetHourlyForecastUseCase(
+    private val repository: WeatherRepository
+) {
+    operator fun invoke(lat: Double, lon: Double): Flow<Result<List<HourlyWeather>>> =
+        repository.getHourlyForecast(lat, lon)
+}
