b80d851e9ebddaf5003ee92bde5e7bc5219dbd69
yasse <yassenyasooo50@gmail.com>
2026-03-10 23:44:05 +0200

    upgrade database version

--- app/src/main/java/com/example/weather_app/data/database/AppDataBae.kt
+++ app/src/main/java/com/example/weather_app/data/database/AppDataBae.kt
@@ -2,14 +2,18 @@ package com.example.weather_app.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.weather_app.data.weather.local.ForecastDao
 import com.example.weather_app.data.weather.local.WeatherDao
+import com.example.weather_app.data.weather.local.entity.DailyForecastEntity
+import com.example.weather_app.data.weather.local.entity.HourlyWeatherEntity
 import com.example.weather_app.data.weather.local.entity.WeatherEntity
 
 @Database(
-    entities = [WeatherEntity::class],
-    version = 2,
+    entities = [WeatherEntity::class, HourlyWeatherEntity::class, DailyForecastEntity::class],
+    version = 3,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun weatherDao(): WeatherDao
+    abstract fun forecastDao(): ForecastDao
 }
