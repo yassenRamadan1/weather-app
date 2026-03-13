9e89e0686856e8c45fc57f9986e431ddcfe336d5
yasse <yassenyasooo50@gmail.com>
2026-03-13 21:24:30 +0200

    add configuration of work manger and schedule module in app

--- app/src/main/java/com/example/weather_app/WeatherApp.kt
+++ app/src/main/java/com/example/weather_app/WeatherApp.kt
@@ -1,13 +1,23 @@
 package com.example.weather_app
 
 import android.app.Application
+import androidx.work.Configuration
+import com.example.weather_app.data.weather.notification.AlertNotificationManager
 import com.example.weather_app.di.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class WeatherApp : Application() {
+class WeatherApp : Application(), Configuration.Provider {
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setMinimumLoggingLevel(android.util.Log.DEBUG)
+            .build()
+
     override fun onCreate() {
         super.onCreate()
+        AlertNotificationManager.createChannels(this)
+
         startKoin {
             androidContext(this@WeatherApp)
             modules(
@@ -17,7 +27,8 @@ class WeatherApp : Application() {
                 repositoryModule,
                 useCaseModule,
                 viewModelModule,
-                locationModule
+                locationModule,
+                schedulerModule
             )
         }
     }
