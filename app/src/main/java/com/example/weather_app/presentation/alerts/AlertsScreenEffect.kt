75870f019291c4d25ff8c5f9b722ea4bbb5d73b0
yasse <yassenyasooo50@gmail.com>
2026-03-13 21:27:06 +0200

    create alert screen ui effect and ui state

---
+++ app/src/main/java/com/example/weather_app/presentation/alerts/AlertsScreenEffect.kt
@@ -0,0 +1,9 @@
+package com.example.weather_app.presentation.alerts
+
+import com.example.weather_app.presentation.uierror.UiText
+
+sealed interface AlertsScreenEffect {
+    data class ShowSnackbar(val message: UiText) : AlertsScreenEffect
+    data object RequestNotificationPermission : AlertsScreenEffect
+    data object RequestExactAlarmPermission   : AlertsScreenEffect
+}
