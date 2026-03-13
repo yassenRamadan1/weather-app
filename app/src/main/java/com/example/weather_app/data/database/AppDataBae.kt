829c32ab76bba601f91f3bf5b8f1e7b94ad248ac
yasse <yassenyasooo50@gmail.com>
2026-03-13 21:16:40 +0200

    improve the version of db

--- app/src/main/java/com/example/weather_app/data/database/AppDataBae.kt
+++ app/src/main/java/com/example/weather_app/data/database/AppDataBae.kt
@@ -4,19 +4,22 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.weather_app.data.weather.local.dao.FavoriteLocationDao
 import com.example.weather_app.data.weather.local.dao.ForecastDao
+import com.example.weather_app.data.weather.local.dao.WeatherAlertDao
 import com.example.weather_app.data.weather.local.dao.WeatherDao
 import com.example.weather_app.data.weather.local.entity.DailyForecastEntity
 import com.example.weather_app.data.weather.local.entity.FavoriteLocationEntity
 import com.example.weather_app.data.weather.local.entity.HourlyWeatherEntity
+import com.example.weather_app.data.weather.local.entity.WeatherAlertEntity
 import com.example.weather_app.data.weather.local.entity.WeatherEntity
 
 @Database(
-    entities = [WeatherEntity::class, HourlyWeatherEntity::class, DailyForecastEntity::class, FavoriteLocationEntity::class],
-    version = 5,
+    entities = [WeatherEntity::class, HourlyWeatherEntity::class, DailyForecastEntity::class, FavoriteLocationEntity::class, WeatherAlertEntity::class],
+    version = 7,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun weatherDao(): WeatherDao
     abstract fun forecastDao(): ForecastDao
     abstract fun favoriteLocationDao(): FavoriteLocationDao
+    abstract fun weatherAlertDao(): WeatherAlertDao
 }
